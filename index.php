1dc83c8c0909bb25f046ad22361d18bf063cd15d
Your Name <yan.vi193@gmail.com>
2022-02-16 12:59:50 +0100

    creazione-file

---
+++ index.php
@@ -0,0 +1,23 @@
+<!--
+    Riscrivere questa pagina del sito google
+    https://policies.google.com/faq.
+    Ci sono diverse domande con relative risposte.
+    Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+-->
+
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
